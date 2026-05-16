8c956860b53fa1312b8e2931ed54295ab1ad061b
Artur Afonso Amaro Araújo <arturafonso4a@gmail.com>
2026-05-16 16:21:41 -0300

    feat: auto-generate ULID on Project creation

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,12 +8,24 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
 
 #[Fillable(['name'])]
 class Project extends Model
 {
     /** @use HasFactory<ProjectFactory> */
     use HasFactory;
+
+    protected static function boot(): void
+    {
+        parent::boot();
+
+        static::creating(function (Project $project) {
+            if (empty($project->ulid)) {
+                $project->ulid = (string) Str::ulid();
+            }
+        });
+    }
 
     public function getRouteKeyName(): string
     {
