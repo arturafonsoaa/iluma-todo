e23dc4fe2b805506a23d6eaca5a738bcac5f3b8e
Artur Afonso Amaro Araújo <arturafonso4a@gmail.com>
2026-05-16 15:02:36 -0300

    fix: clear cached config before tests to prevent wiping dev database

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,33 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Laravel\Fortify\Features;
 
 abstract class TestCase extends BaseTestCase
 {
+    public function createApplication()
+    {
+        $cachedConfig = $this->getCachedConfigPath();
+
+        if (file_exists($cachedConfig)) {
+            unlink($cachedConfig);
+        }
+
+        return parent::createApplication();
+    }
+
+    private function getCachedConfigPath(): string
+    {
+        return $this->appBasePath().'/bootstrap/cache/config.php';
+    }
+
+    private function appBasePath(): string
+    {
+        return Application::inferBasePath();
+    }
+
     protected function skipUnlessFortifyHas(string $feature, ?string $message = null): void
     {
         if (! Features::enabled($feature)) {
